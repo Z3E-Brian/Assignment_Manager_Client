5cbea7aaab957c9992faba8a58e7b5c3b9fa92ec
JustinMdz <justin.mendez.mena@est.una.ac.cr>
2024-10-14 10:10:17 -0600

    [UPDATE] Update UniversityMantainanceView

--- src/main/java/org/una/programmingIII/Assignment_Manager_Client/Controller/UniversityMaintenanceController.java
+++ src/main/java/org/una/programmingIII/Assignment_Manager_Client/Controller/UniversityMaintenanceController.java
@@ -1,78 +1,72 @@
 package org.una.programmingIII.Assignment_Manager_Client.Controller;
 
+
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.input.KeyEvent;
-import org.una.programmingIII.Assignment_Manager_Client.Model.UniversityDto;
 import org.una.programmingIII.Assignment_Manager_Client.Util.Controller;
 
-public class UniversityMaintenanceController  extends Controller  {
+public class UniversityMaintenanceController extends Controller {
+    @FXML
+    private MFXButton btnDelete;
 
-        @FXML
-        private TableColumn<UniversityDto, String> LocationColumn;
+    @FXML
+    private MFXButton btnEdit;
 
-        @FXML
-        private MFXButton btnBuscar;
+    @FXML
+    private MFXButton btnNew;
 
-        @FXML
-        private MFXButton btnEliminar;
+    @FXML
+    private MFXButton btnSave;
 
-        @FXML
-        private MFXButton btnGuardar;
+    @FXML
+    private TableColumn<?, ?> idColumn;
 
-        @FXML
-        private MFXButton btnNuevo;
+    @FXML
+    private MFXTextField idTxf;
 
-        @FXML
-        private TableColumn<UniversityDto, Long> idColumn;
+    @FXML
+    private TableColumn<?, ?> locationColumn;
 
-        @FXML
-        private TableColumn<UniversityDto, String> nameColumn;
+    @FXML
+    private TableColumn<?, ?> nameColumn;
 
-        @FXML
-        private MFXTextField txfId;
+    @FXML
+    private MFXTextField txfLocation;
 
-        @FXML
-        private MFXTextField txfLocation;
+    @FXML
+    private MFXTextField txfName;
 
-        @FXML
-        private MFXTextField txfName;
-
-        @FXML
-        private TableView<?> universityTable;
-
-        @FXML
-        void onActionBtnBuscar(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onActionBtnEliminar(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onActionBtnGuardar(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onActionBtnNuevo(ActionEvent event) {
-
-        }
-
-        @FXML
-        void onKeyPressedTxfId(KeyEvent event) {
-
-        }
-
+    @FXML
+    private TableView<?> universityTable;
 
     @Override
     public void initialize() {
 
     }
+
+    @FXML
+    void onActionBtnEditFaculties(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnNew(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnSave(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionBtnDelete(ActionEvent event) {
+
+    }
+
+
 }
