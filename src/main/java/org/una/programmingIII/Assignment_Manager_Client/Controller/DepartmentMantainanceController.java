7f0a3eb60ac6ff83abc3fd967c743949576f3c31
JustinMdz <justin.mendez.mena@est.una.ac.cr>
2024-10-21 10:06:25 -0600

    [UPDATE] Update deparment mantaince view and controller

--- src/main/java/org/una/programmingIII/Assignment_Manager_Client/Controller/DepartmentMantainanceController.java
+++ src/main/java/org/una/programmingIII/Assignment_Manager_Client/Controller/DepartmentMantainanceController.java
@@ -2,14 +2,26 @@ package org.una.programmingIII.Assignment_Manager_Client.Controller;
 
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import org.una.programmingIII.Assignment_Manager_Client.Dto.DepartmentDto;
+import org.una.programmingIII.Assignment_Manager_Client.Dto.FacultyDto;
+import org.una.programmingIII.Assignment_Manager_Client.Service.DepartmentService;
+import org.una.programmingIII.Assignment_Manager_Client.Service.FacultyService;
+import org.una.programmingIII.Assignment_Manager_Client.Util.AppContext;
 import org.una.programmingIII.Assignment_Manager_Client.Util.Controller;
+import org.una.programmingIII.Assignment_Manager_Client.Util.Message;
+
+import java.util.List;
 
 public class DepartmentMantainanceController extends Controller {
 
@@ -23,10 +35,10 @@ public class DepartmentMantainanceController extends Controller {
     private MFXButton btnSave;
 
     @FXML
-    private TableColumn<?, ?> clmDepartment;
+    private TableColumn<DepartmentDto, String> clmDepartment;
 
     @FXML
-    private TableColumn<?, ?> clmnId;
+    private TableColumn<DepartmentDto, Long> clmnId;
 
     @FXML
     private ImageView imvBack;
@@ -41,7 +53,7 @@ public class DepartmentMantainanceController extends Controller {
     private Label lblFaculty;
 
     @FXML
-    private TableView<?> tbvDepartment;
+    private TableView<DepartmentDto> tbvDepartment;
 
     @FXML
     private MFXTextField txfDepartmentName;
@@ -49,45 +61,125 @@ public class DepartmentMantainanceController extends Controller {
     @FXML
     private MFXTextField txfId;
 
+    private FacultyService facultyService;
+    private DepartmentService departmentService;
+    private FacultyDto facultyDto;
+    private DepartmentDto departmentDto;
+
+
     @Override
     public void initialize() {
-        System.out.println("Initialize DepartmentMantainanceController");
+        facultyService = new FacultyService();
+        departmentService = new DepartmentService();
+        facultyDto = new FacultyDto();
+        departmentDto = new DepartmentDto();
+        updateUniversityInputLabel();
+
+        clmnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        clmDepartment.setCellValueFactory(new PropertyValueFactory<>("name"));
+        tbvDepartment.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
+        txfId.setDisable(true);
+        loadDepartments();
     }
 
     @FXML
     void OnMousePressedTbvDepartment(MouseEvent event) {
-
+        if (event.isPrimaryButtonDown() && event.getClickCount() == 1) {
+            departmentDto = tbvDepartment.getSelectionModel().getSelectedItem();
+            bind();
+        }
     }
 
     @FXML
-    void onActionBtnDelete(ActionEvent event) {
-
+    void onActionBtnDelete(ActionEvent event) throws Exception {
+        if (!(txfId.getText().isEmpty())) {
+            departmentService.deleteDepartment(departmentDto.getId());
+            clean();
+            loadDepartments();
+        } else {
+            new Message().showModal(Alert.AlertType.WARNING, "Eliminar departamento", getStage(), "Debe selecionar uno de los departamentos en la tabla para poder eliminarlo.");
+        }
     }
 
     @FXML
     void onActionBtnNewDepartment(ActionEvent event) {
-
+        clean();
     }
 
     @FXML
-    void onActionBtnSave(ActionEvent event) {
-
+    void onActionBtnSave(ActionEvent event) throws Exception {
+        if (!(txfId.getText().isBlank())) {
+            updateDepartment();
+        } else {
+            createDepartment();
+        }
     }
 
     @FXML
     void onMouseClickedImvBack(MouseEvent event) {
-
+        System.out.println("imvBack");
     }
 
     @FXML
     void onMouseClickedImvClose(MouseEvent event) {
-
+        System.out.println("imvClose");
     }
 
     @FXML
     void onMouseClickedImvSearch(MouseEvent event) {
-
+        System.out.println("imvSearch");
     }
 
+    private void createDepartment() throws Exception {
+        if ((txfDepartmentName.getText().isBlank())) {
+            new Message().showModal(Alert.AlertType.ERROR, "Crear departamento", getStage(), "Existen espacios  importantes en blanco");
+        } else {
+            departmentDto = new DepartmentDto();
+            departmentDto.setName(txfDepartmentName.getText());
+            departmentDto.setFacultyId(facultyDto.getId());
+            departmentDto = departmentService.createDepartment(departmentDto);
+            clean();
+            loadDepartments();
+        }
+    }
+
+    private void updateDepartment() throws Exception {
+        if ((txfDepartmentName.getText().isBlank())) {
+            new Message().showModal(Alert.AlertType.ERROR, "Actualizar Departamento", getStage(), "Existen espacios  importantes en blanco");
+        } else {
+            departmentDto.setName(txfDepartmentName.getText());
+            departmentDto = departmentService.updateDepartment(departmentDto.getId(), departmentDto);
+            clean();
+            loadDepartments();
+        }
+    }
+
+    private void updateUniversityInputLabel() {
+        facultyDto = (FacultyDto) AppContext.getInstance().get("facultyDto");
+        lblFaculty.setText(facultyDto.getName());
+    }
+
+    private void loadDepartments() {
+        try {
+            facultyDto = facultyService.getFacultyById(facultyDto.getId());
+            List<DepartmentDto> departmentDtoList = facultyDto.getDepartments();
+            ObservableList<DepartmentDto> departmentDtoObservableList = FXCollections.observableArrayList(departmentDtoList);
+            tbvDepartment.setItems(departmentDtoObservableList);
+        } catch (Exception e) {
+            new Message().showModal(Alert.AlertType.WARNING, "Error de conexion", getStage(), "Debe selecionar una de las universidades en la tabla para poder eliminarla.");
+
+        }
+    }
+
+    private void clean() {
+        this.txfDepartmentName.clear();
+        this.txfId.clear();
+        departmentDto = new DepartmentDto();
+    }
+
+    private void bind() {
+        txfId.setText(departmentDto.getId().toString());
+        txfDepartmentName.setText(departmentDto.getName());
+    }
 
 }
---
+++ src/main/java/org/una/programmingIII/Assignment_Manager_Client/Service/DepartmentService.java
@@ -0,0 +1,74 @@
+package org.una.programmingIII.Assignment_Manager_Client.Service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.una.programmingIII.Assignment_Manager_Client.Dto.DepartmentDto;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+
+public class DepartmentService {
+    private static final String BASE_URL = "http://localhost:8080/api/departments";
+    private final HttpClient httpClient;
+    private final ObjectMapper objectMapper;
+
+    public DepartmentService() {
+        this.httpClient = HttpClient.newHttpClient();
+        this.objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+    }
+
+    public DepartmentDto createDepartment(DepartmentDto departmentDto) throws Exception {
+        String requestBody = objectMapper.writeValueAsString(departmentDto);
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/create"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 201) {
+            return objectMapper.readValue(response.body(), DepartmentDto.class);
+        } else {
+            throw new Exception("Error creating Department: " + response.statusCode());
+        }
+    }
+
+    public DepartmentDto updateDepartment(Long id, DepartmentDto departmentDto) throws Exception {
+        String requestBody = objectMapper.writeValueAsString(departmentDto);
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/" + id))
+                .header("Content-Type", "application/json")
+                .PUT(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() == 200) {
+            return objectMapper.readValue(response.body(), DepartmentDto.class);
+        } else if (response.statusCode() == 404) {
+            throw new Exception("Department not found");
+        } else {
+            throw new Exception("Error updating Departmen: " + response.statusCode());
+        }
+    }
+
+    public void deleteDepartment(Long id) throws Exception {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(BASE_URL + "/" + id))
+                .DELETE()
+                .build();
+
+        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        if (response.statusCode() != 204) {
+            throw new Exception("Error deleting department: " + response.statusCode());
+        }
+    }
+}
+
